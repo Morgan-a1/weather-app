0cada9fb7262946ce4b98e788bf8c49cbd370906
Morgan <morgan_a1@hotmail.com>
2021-12-16 23:18:06 +0100

    Added almost all functionality

--- src/helpers/getUpcomingDaysForecast.js
+++ src/helpers/getUpcomingDaysForecast.js
@@ -5,7 +5,8 @@ const getWeekday = date => moment(date).format('dddd').substring(0, 3);
 const getUpcomingDaysForecast = data =>
     data.slice(1).map(day => ({
         imgUrl: day.weather_state_abbr,
-        temperature: Math.round(day.max_temp),
+        // temperature: Math.round(day.max_temp),
+        temperature: Math.round(day.the_temp),
         weekday: getWeekday(day.applicable_date),
     }));
 
